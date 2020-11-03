4366c866f2c57743934a84e530e9e014971a12db
firat ulas <fulas@gmail.com>
2020-11-03 13:01:57 +0100

    the fifth commit from local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -7,12 +7,9 @@ public class FirstClass {
 		System.out.println("This is the second changes from local master");		
 		System.out.println("This is the third changes from local master");	
 		System.out.println("This is the first commit from remote master");
-<<<<<<< HEAD
+
 		System.out.println("This is the forth commit from local master");
-	}}
-=======
-		
-		System.out.println("This is the second commit from remote master");
+ 
+        System.out.println("This is the second commit from remote master");
 	
 	}}
->>>>>>> 2ea58ffc9da22df2e4022cadc92c98a6a5d6d23e
